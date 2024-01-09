65bc636c6002e7abd70f6b403f9eaeb2c41b056e
bmamaxim <bma@gmail.com>
2024-01-09 21:00:58 +0500

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title text,
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id text PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
+
+CREATE TABLE orders
+(
+	order_id int,
+	customer_id text references customers(customer_id),
+	employee_id int references employees(employee_id),
+	order_date date,
+	ship_city text
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,47 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+# соединениие с базой данных
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='123456'
+)
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open("north_data/employees_data.csv", 'r', newline='') as file:
+                employees = csv.reader(file)
+                next(employees)
+                for employee in employees:
+                    cur.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes)"
+                        " VALUES (%s,%s, %s, %s, %s, %s)",
+                        employee)
+
+        with conn.cursor() as cur:
+            with open("north_data/customers_data.csv", 'r', newline='') as file:
+                customers = csv.reader(file)
+                next(customers)
+                for customer in customers:
+                    cur.execute(
+                        "INSERT INTO customers (customer_id, company_name, contact_name)"
+                        " VALUES (%s,%s, %s)",
+                        customer)
+
+        with conn.cursor() as cur:
+            with open("north_data/orders_data.csv", 'r', newline='') as file:
+                orders = csv.reader(file)
+                next(orders)
+                for order in orders:
+                    cur.execute(
+                        "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city)"
+                        " VALUES (%s, %s, %s, %s, %s)",
+                        order)
+
+finally:
+    conn.close()
